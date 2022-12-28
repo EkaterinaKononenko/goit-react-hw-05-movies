ec8f5e8a35fa93b5915c4c2188d290daaed65a8a
EkaterinaKononenko <kaktysik4@ukr.net>
2022-12-28 17:06:20 +0200

    add movie details

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,5 +1,5 @@
 
-import { NavLink, Outlet, useParams, useNavigate, useLocation } from 'react-router-dom';
+/*import { NavLink, Outlet, useParams, useNavigate, useLocation } from 'react-router-dom';
 import getMoviesDetails from 'API/getMoviesDetails';
 import { useState, useEffect } from 'react';
 
@@ -36,7 +36,7 @@ const MovieDetails = ({
     <div>
       <div>
         <button type="button" onClick={onGoBackClick}>
-          GO BACK
+           GO BACK
         </button>
       </div>
       Now showing product with id {id}
@@ -57,7 +57,74 @@ const MovieDetails = ({
   );
 };
 
+export default MovieDetails;*/
+
+import {
+  NavLink,
+  Outlet,
+  useParams,
+  useNavigate,
+  useLocation,
+} from 'react-router-dom';
+import getMoviesDetails from 'API/getMoviesDetails';
+import { useState, useEffect } from 'react';
+
+const MovieDetails = () => {
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const pathname = 'https://image.tmdb.org/t/p/w500';
+
+  useEffect(() => {
+    getMoviesDetails(id)
+      .then(setMovie)
+      .catch(error => {
+        console.log(error);
+      });
+  }, [id]);
+
+  if (!movie) {
+    return null;
+  }
+
+  const onGoBackClick = () => {
+    navigate(location?.state?.from ?? '/');
+  };
+  const { title, vote_average, overview, genres, poster_path } = movie;
+  const vote = Math.round(vote_average);
+  const genreSet = genres.map(ganre => ganre.name).join(', ');
+
+  return (
+    <div>
+      <div>
+        <button type="button" onClick={onGoBackClick}>
+          GO BACK
+        </button>
+      </div>
+      <div>
+        <img src={pathname + poster_path} alt='movie' />
+      </div>
+      <h1>{title}</h1>
+      <p>vote: {vote}</p>
+      <p>{overview}</p>
+      <p>{genreSet}</p>
+      <ul>
+        <li>
+          <NavLink to="cast">CAST</NavLink>
+        </li>
+        <li>
+          <NavLink to="rewievs">REWIEVS</NavLink>
+        </li>
+      </ul>
+      <Outlet />
+    </div>
+  );
+};
+
 export default MovieDetails;
+
+
 
     
 
